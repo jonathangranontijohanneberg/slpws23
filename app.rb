80fd1cb03ecc84663d694a72d852da9248e3a54f
jonathangranontijohanneberg <93259651+jonathangranontijohanneberg@users.noreply.github.com>
2023-02-07 11:52:27 +0100

    Gjorde en model.rb för snyggare kod där jag sammanställde några metoder för enkelhet. Även ett helper-block använder jag nu för att kunna få ut allt inom visual_novel och för att kunna få ut namnet på ett element i en given tabell mha ett givet id på elementet.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 require 'sinatra/reloader'
+require_relative './model.rb'
 
 enable :sessions
 
@@ -18,21 +19,17 @@ post('/visual_novel/new') do
     name = params[:name]
     genre = params[:genre]
     description = params[:description]
+    creator = params[:creator]
 
     # user_id = session[:id].to_i
     db = SQLite3::Database.new("db/db.db")
-    genre_id_arr = db.execute("SELECT id FROM genre WHERE name = ?", genre)
-# ALL genre_id
-    if genre_id_arr.empty?
-        highest_genre_id = db.execute("SELECT MAX(id) FROM genre")
 
-        genre_id = highest_genre_id[0][0].to_i + 1
+    genre_id = attribute_id(genre, "genre", db)
+    creator_id = attribute_id(creator, "creator", db)
+    visual_novel_id = db.execute("SELECT id FROM visual_novel WHERE name=?", name)
 
-        db.execute("INSERT INTO genre (name, id) VALUES (?,?)", genre, genre_id)
-    else
-        genre_id = genre_id_arr[0][0].to_i
 
-    end
+    # ALL genre_id
         
     # "INSERT INTO visual_novel (name, genre, description) VALUES (?,?, ?)", name, genre_id, text)
 
@@ -40,8 +37,13 @@ post('/visual_novel/new') do
     p name
     p genre_id
     p description
+    p creator_id
+    p visual_novel_id
 
-    db.execute("INSERT INTO visual_novel (name, genre_id, text) VALUES (?,?,?)", name, genre_id, description)
+    db.execute("INSERT INTO visual_novel (name, genre_id, text, creator_id) VALUES (?,?,?,?)", name, genre_id, description, creator_id)
+    # OCH LÄGG TILL ID:ENA I RELATIONSTABELLERNA!!!!!!
+    db.execute("INSERT INTO visual_novel_creator_relation (visual_novel_id, creator_id) VALUES (?,?)", visual_novel_id, creator_id)
+
     redirect("/visual_novel")
 end
 
@@ -50,6 +52,36 @@ get('/visual_novel') do
     db = SQLite3::Database.new('db/db.db')
     db.results_as_hash = true
     result = db.execute("SELECT * FROM visual_novel")
+    genre_result = db.execute("SELECT * FROM genre")
+    p "genre_result"
+    p genre_result
+    # ha med även genre-tabellen så att man kan inkludera genre- och skapar-texten och inte bara id:et
+    # `?????????????????????????????????????????????????`
     # "WHERE user_id = ?",id
+
     slim(:"visual_novel/index", locals:{visual_novel:result})
+end
+
+helpers do
+    # def initialize
+    #     @db = SQLite3::Database.new('db/db.db')
+    #     @db.results_as_hash = true
+    # end
+
+    def all_visual_novels
+        db = SQLite3::Database.new('db/db.db')
+        db.results_as_hash = true
+
+        result = db.execute("SELECT * FROM visual_novel")
+        return result
+    end
+
+    def name_with_id(table, id)
+        db = SQLite3::Database.new('db/db.db')
+        db.results_as_hash = true
+
+        name = db.execute("SELECT name FROM #{table} WHERE id=?",id)[0]["name"]
+        return name
+    end
+     
 end
---
+++ model.rb
@@ -0,0 +1,15 @@
+
+
+def attribute_id(attribute, table, db)
+    attribute_id_arr = db.execute("SELECT id FROM #{table} WHERE name = ?", attribute)
+
+    if attribute_id_arr.empty?
+        highest_id = db.execute("SELECT MAX(id) FROM #{table}")
+        attribute_id = highest_id[0][0].to_i + 1
+        db.execute("INSERT INTO #{table} (name, id) VALUES (?,?)", attribute, attribute_id)
+    else
+        attribute_id = attribute_id_arr[0][0].to_i
+    end
+
+    return attribute_id
+end
